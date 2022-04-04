cb20cbdd75c51313e3166e1cb546e72e954a48f9
Rakesh Kumar Sharma <rakeshsharma715078@gmail.com>
2022-04-05 01:58:06 +0530

    Project Done phase 1

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 
-mongoose.connect("mongodb+srv://arnabbiswas_14:arnabbiswas@cluster0.b95gv.mongodb.net/group-x-Database?retryWrites=true&w=majority" , {
+mongoose.connect("mongodb+srv://sumandev:aBosU15RXTGZYkKq@cluster0.4du2i.mongodb.net/group37database?retryWrites=true&w=majority" , {
     useNewUrlParser:true
 })
 
